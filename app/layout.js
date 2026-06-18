ef16ec299842296a28312c52e7efc84a85004c37
vishalchhalotre123-del <vishalchhalotre9340@gmail.com>
2026-06-18 11:15:05 +0530

    2nd commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/Navbar/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/platform/page.js
@@ -0,0 +1,6 @@
+export default function PlatformPage() {
+  return (
+    <main>
+    </main>
+  );
+}
